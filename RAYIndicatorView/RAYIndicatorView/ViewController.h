fe0e57cc1f4e0b4378ab798dcd978276b6970231
raydang <17151328@qq.com>
2015-05-26 17:15:24 +0800

    create project

---
+++ RAYIndicatorView/RAYIndicatorView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RAYIndicatorView
+//
+//  Created by richerpay on 15/5/26.
+//  Copyright (c) 2015年 richerpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
